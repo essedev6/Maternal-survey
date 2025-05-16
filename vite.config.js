2cc5e39ebfc2347bd697c8e61faacc036e64a30f
Jesse Nyangao <jessenyangaob@gmail.com>
2025-05-16 13:38:11 +0300

    vite.confog updaten

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://maternal-survey-backend.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
