7c06c572362b66ea48bcb49f3b93a6cc6f85f71e
Jesse Nyangao <jessenyangaob@gmail.com>
2025-05-16 14:22:50 +0300

    vercel errorn

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure base URL is root
+  build: {
+    outDir: 'dist', // Where build files go
+    emptyOutDir: true // Clear folder before build
+  },
   server: {
     proxy: {
       '/api': {
